f83cec08ce6013262839e9667d4179cc6157276a
filtercake <fabian@superpursuitmo.de>
2012-10-21 20:31:16 +0200

    snapshot

---
+++ assets/javascripts/ixd.js
@@ -0,0 +1,1 @@
+$(".exp").click(function(){$(".tile2, .tile4").removeClass("expanded"),$(".tile1",".tile2",".tile4").removeClass("exp"),$(this).addClass("expanded"),$(this).removeClass("exp")}),$("#link-to-impressum").click(function(){$("#impressum").show()});
